8a4f51310aafb019a565ffb27bbeec3d7165d6ee
ieatbluepencils <ieatmassivebluepencils@gmail.com>
2026-08-10 19:57:11 +1000

    write tests for expression compilation

--- compiler/tests/test_compiler.rs
+++ compiler/tests/test_compiler.rs
@@ -2,18 +2,16 @@ use compiler::Compiler;
 use runtime::{chunk::Chunk, value::Value};
 
 #[test]
-fn compile_arithmetic_exprs() {
+fn test_compile_stmtexpr() {
     let mut compiler = Compiler::new();
-    // unwrap cuz it shouldnt error and we dont care
-    let chunk = compiler.compile(b"(1 + 2) * 3;").unwrap();
-
+    let chunk = compiler.compile(b"1 + 2 * 3;").unwrap();
     let constants = vec![
         Value::new_int(1),
         Value::new_int(2),
         Value::new_int(3),
         Value::new_int(0),
     ];
-    let bytecode = vec![6, 0, 0, 0, 6, 1, 0, 0, 1, 6, 2, 0, 0, 3, 6, 3, 0, 0, 0];
+    let bytecode = vec![6, 0, 0, 0, 6, 1, 0, 0, 6, 2, 0, 0, 3, 1, 8, 6, 3, 0, 0, 0];
 
     assert_eq!(chunk, Chunk::new_from_existing(bytecode, constants));
 }
