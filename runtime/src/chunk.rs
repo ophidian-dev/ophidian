c145675270bb7e6417314128dba128ddff89c81e
ieatbluepencils <ieatmassivebluepencils@gmail.com>
2026-08-22 14:35:57 +1000

    add jumping functions for chunk

--- runtime/src/chunk.rs
+++ runtime/src/chunk.rs
@@ -1,4 +1,5 @@
 use crate::value::Value;
+use crate::opcodes::OpCode;
 
 #[derive(Debug, PartialEq)]
 pub struct Chunk {
@@ -31,6 +32,28 @@ impl Chunk {
         self.write((u24 & 0xFF) as u8);
         self.write(((u24 >> 8) & 0xFF) as u8);
         self.write(((u24 >> 16) & 0xFF) as u8);
+    }
+
+    pub fn write_i32(&mut self, i32: i32) {
+        for byte in i32.to_le_bytes() {
+            self.write(byte);
+        }
+    }
+
+    pub fn write_jump(&mut self, opcode: OpCode) -> usize {
+        let pos = self.bytecode.len();
+        self.write(opcode as u8);
+        self.write_i32(0);
+        pos
+    }
+
+    pub fn patch_jump(&mut self, pos: usize) {
+        let target = self.bytecode.len();
+
+        let offset = target as i32 - (pos as i32 + 5);
+
+        let bytes = offset.to_le_bytes();
+        self.bytecode[pos + 1..pos + 5].copy_from_slice(&bytes);
     }
 
     pub fn write_constant(&mut self, constant: Value) -> usize {
