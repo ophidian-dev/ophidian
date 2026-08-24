fa6317892033beabe3f435024f3d261b23be22d3
ieatbluepencils <ieatmassivebluepencils@gmail.com>
2026-08-24 21:07:07 +1000

    move non parsing functions out of parser.rs

--- frontend/src/parse/mod.rs
+++ frontend/src/parse/mod.rs
@@ -1,4 +1,107 @@
 pub mod ast;
 mod parser;
 
-pub use parser::Parser;
+use crate::diagnostics::{Diagnostic, Severity};
+use crate::lex::token::{Token, TokenKind, TokenStream};
+use crate::span::Span;
+use crate::parse::ast::NodeId;
+
+pub struct Parser<'src, 'diag, T>
+where
+    T: TokenStream,
+{
+    // a stream of tokens that we can iterate over by calling `.next()` on it
+    tokenstream: T,
+
+    // vector of diagnostics
+    pub diagnostics: &'diag mut Vec<Diagnostic>,
+
+    // a reference to the source string
+    source: &'src [u8],
+
+    // the current token
+    current: Token,
+    // previous token
+    prev: Token,
+
+    // current nodeid
+    curr_nodeid: NodeId,
+}
+
+impl<'src, 'diag, T: TokenStream> Parser<'src, 'diag, T> {
+    pub fn new(
+        mut tokenstream: T,
+        diagnostics: &'diag mut Vec<Diagnostic>,
+        source: &'src [u8],
+    ) -> Self {
+        let current = tokenstream.next();
+        Self {
+            tokenstream,
+            diagnostics,
+            current,
+            prev: Token::dummy(),
+            curr_nodeid: NodeId(0),
+            source,
+        }
+    }
+
+    fn peek(&self) -> Token {
+        self.current
+    }
+
+    fn advance(&mut self) -> Token {
+        self.prev = self.current;
+        self.current = self.tokenstream.next();
+        self.prev
+    }
+
+    fn next_node_id(&mut self) -> NodeId {
+        let tmp = self.curr_nodeid;
+        self.curr_nodeid.increment();
+        tmp
+    }
+
+    fn error<M: Into<String>>(&mut self, message: M, span: Span) {
+        self.diagnostics
+            .push(Diagnostic::new(message.into(), span, Severity::Error));
+    }
+
+     fn sync(&mut self) {
+        while self.peek().kind != TokenKind::Eof {
+            let kind = self.peek().kind;
+            match kind {
+                TokenKind::Semicolon => {
+                    self.advance();
+                    return;
+                }
+                TokenKind::Print => {
+                    return;
+                }
+                TokenKind::Let => {
+                    return;
+                }
+                TokenKind::If => {
+                    return;
+                }
+                TokenKind::While => {
+                    return;
+                }
+                TokenKind::For => {
+                    return;
+                }
+                TokenKind::Break => {
+                    return;
+                }
+                TokenKind::Continue => {
+                    return;
+                }
+                TokenKind::OpenBrace => {
+                    return;
+                }
+                _ => {
+                    self.advance();
+                }
+            }
+        }
+    }
+}
--- frontend/src/parse/parser.rs
+++ frontend/src/parse/parser.rs
@@ -1,110 +1,12 @@
 use crate::analysis::analyzer::Type;
-use crate::diagnostics::{Diagnostic, Severity};
-use crate::lex::token::{Token, TokenKind, TokenStream};
+use crate::lex::token::{TokenKind, TokenStream};
 use crate::parse::ast::{
-    BinOpKind, Expr, ExprKind, ForInit, LitKind, NodeId, Program, Stmt, StmtKind, UnaryOpKind,
+    BinOpKind, Expr, ExprKind, ForInit, LitKind, Program, Stmt, StmtKind, UnaryOpKind,
 };
+use crate::parse::Parser;
 use crate::span::{Span, Spanned};
 
-pub struct Parser<'src, 'diag, T>
-where
-    T: TokenStream,
-{
-    // a stream of tokens that we can iterate over by calling `.next()` on it
-    tokenstream: T,
-
-    // vector of diagnostics
-    pub diagnostics: &'diag mut Vec<Diagnostic>,
-
-    // a reference to the source string
-    source: &'src [u8],
-
-    // the current token
-    current: Token,
-    // previous token
-    prev: Token,
-
-    // current nodeid
-    curr_nodeid: NodeId,
-}
-
 impl<'src, 'diag, T: TokenStream> Parser<'src, 'diag, T> {
-    pub fn new(
-        mut tokenstream: T,
-        diagnostics: &'diag mut Vec<Diagnostic>,
-        source: &'src [u8],
-    ) -> Self {
-        let current = tokenstream.next();
-        Self {
-            tokenstream,
-            diagnostics,
-            current,
-            prev: Token::dummy(),
-            curr_nodeid: NodeId(0),
-            source,
-        }
-    }
-
-    fn peek(&self) -> Token {
-        self.current
-    }
-
-    fn advance(&mut self) -> Token {
-        self.prev = self.current;
-        self.current = self.tokenstream.next();
-        self.prev
-    }
-
-    fn next_node_id(&mut self) -> NodeId {
-        let tmp = self.curr_nodeid;
-        self.curr_nodeid.increment();
-        tmp
-    }
-
-    fn error<M: Into<String>>(&mut self, message: M, span: Span) {
-        self.diagnostics
-            .push(Diagnostic::new(message.into(), span, Severity::Error));
-    }
-
-    fn sync(&mut self) {
-        while self.peek().kind != TokenKind::Eof {
-            let kind = self.peek().kind;
-            match kind {
-                TokenKind::Semicolon => {
-                    self.advance();
-                    return;
-                }
-                TokenKind::Print => {
-                    return;
-                }
-                TokenKind::Let => {
-                    return;
-                }
-                TokenKind::If => {
-                    return;
-                }
-                TokenKind::While => {
-                    return;
-                }
-                TokenKind::For => {
-                    return;
-                }
-                TokenKind::Break => {
-                    return;
-                }
-                TokenKind::Continue => {
-                    return;
-                }
-                TokenKind::OpenBrace => {
-                    return;
-                }
-                _ => {
-                    self.advance();
-                }
-            }
-        }
-    }
-
     pub fn parse(&mut self) -> Program {
         let mut program = Program::new();
 
@@ -856,7 +758,9 @@ impl<'src, 'diag, T: TokenStream> Parser<'src, 'diag, T> {
 mod tests {
 
     use super::*;
-
+    use crate::diagnostics::Diagnostic;
+    use crate::parse::ast::NodeId;
+    use crate::lex::token::Token;
     use crate::lex::Lexer;
 
     fn new_diag_vec() -> Vec<Diagnostic> {
